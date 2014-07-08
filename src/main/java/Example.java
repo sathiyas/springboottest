0c4f192339604968f0280948796c3fe0c1e7ebe7
Sathiya Shunmugasundaram <sathiyas@users.noreply.github.com>
2014-07-08 08:46:49 -0400

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class Example {
 @RequestMapping("/")
 String home() {
-return "Hello World Sathiyavageswaran!";
+return "Hello World Sathiyavageswaran Shunmugasundaram!";
 }
 public static void main(String[] args) throws Exception {
 SpringApplication.run(Example.class, args);
